6fdabf3a9da129ef915ceb86b89f9566dbafeb8b
KovalenkoMargarita <99732139+KovalenkoMargarita@users.noreply.github.com>
2022-04-04 17:10:14 +0700

    Слайдер с кнопками

--- Задание 10/main.js
+++ Задание 10/main.js
@@ -1,28 +1,49 @@
 let carusels = document.getElementsByClassName('custom-carousel-item');
 
-let btnr = document.getElementsByClassName('carousel-control-next');
-i = 0;
+let slade = 0;
+
 function prev() {
-	carusels[i].style.display = 'none';
-	let counter = document.getElementsByClassName('counter');
-	counter.innerText = --i;
-	console.log(carusels);
+	showSlide(--slade);
 }
 
 function next() {
-	carusels[i].style.display = 'none';
-	let counter = document.getElementsByClassName('counter');
-	counter.innerText = ++i;
-	console.log(carusels);
-}
-
-let btnl = document.getElementsByClassName('carousel-control-prev'); 
- for (let i=0; i< btnl.length;) {
- btnl[i].addEventListener('click', prev);
-}
-
-if(i >= carusels.length){
-  i = 0;
+	showSlide(++slade);
 }
 
 
+const next_btn = document.getElementsByClassName('custom-carousel-control-next')[0];
+const prev_btn = document.getElementsByClassName('custom-carousel-control-prev')[0];
+const dots = document.querySelectorAll('.dot');
+
+
+next_btn.addEventListener('click', next);
+prev_btn.addEventListener('click', prev);
+
+
+function showSlide(i) {
+	let slides = document.getElementsByClassName('custom-carousel-item');
+	let length = slides.length - 1;
+	
+	if (i < 1) {
+		slade = length;
+	}
+	
+	if (i > length) {
+		slade = 0;
+	}
+	
+	for (let element of slides) {
+    element.classList.remove('active');
+  }
+
+  slides[slade].classList.add('active');
+  
+}
+
+//Здесь в чём-то ошибка. Без нижнего кода слайдер работает, но с ним ни кнопки, ни сам слайдер не срабатывает.
+
+dots.forEach((custom-carousel-item, sladeDot) => {
+	custom-carousel-item.addEventListener('click', () => {
+		slade = sladeDot;
+	}
+}
